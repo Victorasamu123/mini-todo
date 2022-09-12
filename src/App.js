7ad73f9ee3f4bd0357634adc3dcf56cc2ce092e8
Asamu victor <asamuvictor03@gmail.com>
2022-09-12 08:25:58 +0100

    added new features to body

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Routes } from 'react-router-dom';
-
+import { Route, Routes } from 'react-router-dom';
+import LandingPage from './components/LandingPage';
 function App() {
+  
   return (
    <>
      <Routes>
-      
+      <Route path='/mytodos' element={<LandingPage/>}/>
      </Routes>
    </>
   );
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,11 +1,16 @@
 import { useNavigate } from "react-router-dom"
-
+import "../styles/LandingPage.css"
 const LandingPage = () => {
   return (
     <>
-     
+    <div>MyTodos</div>
+     <div className="loader">
+        <span></span>
+        <span></span>
+        <span></span>
+     </div>
     </>
   )
 }
 
-export default LandingPage
+export default LandingPage;
